661ef365d07cc4fa9f0d4f6f0223f2a572f12e8c
Katya <86482797+Baytszy@users.noreply.github.com>
2024-06-25 15:44:32 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,19 @@
+
+
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print(my_dict)
+print(my_dict.get('Vasya'))
+print(my_dict.get('Katy'))
+my_dict['Koly'] = 1991
+my_dict['Cat'] = 1995
+print(my_dict)
+a = my_dict.pop('Egor')
+print(my_dict)
+print(a)
+
+#множества
+my_set =  {1,2,1,'груша', 42.314}
+print(my_set)
+my_set.add(10)
+new_set = my_set.add((5, 6, 1.6))
+print(my_set,new_set)
